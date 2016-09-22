d54f2a6bd77ddd2cd68b7aaa2352ebcaaa0243d0
Aaron Krause <aaroncrows@Aarons-MacBook-Pro.local>
2016-09-22 12:25:45 -0700

    update inclass

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -10,7 +10,20 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
+  // TODO: Use handlebars to render your articles!
+  //       - Select your template from the DOM.
+  //       - Now "compile" your template with Handlebars.
 
+  // DONE: If your template will use properties that aren't on the object yet, add them.
+  //   Since your template can't hold any JS logic, we need to execute the logic here.
+  //   The result is added to the object as a new property, which can then be referenced
+  //   by a key in the template. For example, you might want to display how old a post is,
+  //   or say "(draft)" if it has no publication date:
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+
+  // TODO: Use the function that Handlebars gave you to return your filled-in
+  //       html template for THIS article.
 };
 
 ourLocalData.sort(function(a,b) {
